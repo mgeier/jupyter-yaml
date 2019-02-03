f971f0141461aedc17ecedf1c945ef2bde0be7b9
Matthias Geier <Matthias.Geier@gmail.com>
2019-02-03 14:47:25 +0100

    Parallel processing

--- src/jupyter_format/replace_all.py
+++ src/jupyter_format/replace_all.py
@@ -14,20 +14,27 @@ from nbconvert.writers import FilesWriter
 
 
 def convert_to_jupyter(path):
+    path = Path(path)
     exporter = JupyterExporter()
     nb, resources = exporter.from_filename(str(path))
     writer = FilesWriter()
     writer.write(nb, resources, notebook_name=path.with_suffix('').name)
+    path.unlink()
 
 
-def replace_all_recursive(start_dir):
-    for path in Path(start_dir).rglob('*.ipynb'):
-        convert_to_jupyter(path)
-        path.unlink()
+def replace_all_recursive(start_dir, mapfunction=map):
+    notebooks = Path(start_dir).rglob('*.ipynb')
+    for _ in mapfunction(convert_to_jupyter, notebooks):
+        # NB: A lazy mapfunction must be consumed to produce its side effects
+        pass
 
 
 if __name__ == '__main__':
     if set(sys.argv[1:]) != {'--recursive', '--yes'}:
         sys.exit('This replaces all *.ipynb files recursively! '
                  'Use --recursive --yes to consent.')
-    replace_all_recursive('.')
+
+    from concurrent.futures import ProcessPoolExecutor
+
+    with ProcessPoolExecutor() as executor:
+        replace_all_recursive('.', mapfunction=executor.map)
